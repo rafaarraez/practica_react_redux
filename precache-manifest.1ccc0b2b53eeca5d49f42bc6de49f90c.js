7d6f9d978cee851ba16d1b45dbcef2ddc0d2cd12
Rafael Arraez <rafa.arraez.gue@gmail.com>
2021-01-04 15:46:48 -0400

    Updates

---
+++ precache-manifest.1ccc0b2b53eeca5d49f42bc6de49f90c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ddca3dde50d8172b81ff3a1dd9751cc",
+    "url": "/practica_react_redux/index.html"
+  },
+  {
+    "revision": "cc4354580663d8ebe9f3",
+    "url": "/practica_react_redux/static/css/2.f3cffc9e.chunk.css"
+  },
+  {
+    "revision": "951122b2b4faec7cf977",
+    "url": "/practica_react_redux/static/css/main.1b5f1de5.chunk.css"
+  },
+  {
+    "revision": "cc4354580663d8ebe9f3",
+    "url": "/practica_react_redux/static/js/2.8b9b2abf.chunk.js"
+  },
+  {
+    "revision": "e92311d493a1dde02c3c18735aca81e5",
+    "url": "/practica_react_redux/static/js/2.8b9b2abf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "951122b2b4faec7cf977",
+    "url": "/practica_react_redux/static/js/main.ca1e3af6.chunk.js"
+  },
+  {
+    "revision": "d42f91963769ef7efbfd",
+    "url": "/practica_react_redux/static/js/runtime-main.3a038562.js"
+  },
+  {
+    "revision": "f84be7bbe8d8834a4b39c0e632356e67",
+    "url": "/practica_react_redux/static/media/pokemon.f84be7bb.jpg"
+  }
+]);
